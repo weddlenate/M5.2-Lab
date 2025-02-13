061386c0a83a2608e29252f704dd4eff12f94e18
Nate Weddle <nathanweddle@u.boisestate.edu>
2025-02-12 20:16:21 -0700

    Finished

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# web-dev-starter
+# Accessibility Issues
 
-This is a starter project for web development with no frameworks and minimal
-dependencies. It is intended to be a starting point for web development projects
-that are written in plain HTML, CSS, and JavaScript.
+This is a project that gives us a webpage with a number of accessibility issues that we're tasked with improving. We we're given a few specific things to fix and questions to answer, which are located in the Accessibility Lab Answers at the bottom of this document.
 
 ## Getting Started
 
@@ -27,3 +25,44 @@ To run the tests for the project, run the following command:
 ```bash
 npm test
 ```
+
+## Accessibility Lab Answers
+Color:
+    According to Aim's Color Contrast Checker (https://webaim.org/resources/contrastchecker/), the background and foreground, as is originally given, have a constrast ratio of 4.08:1. This does not meet the 4.5:1 standard. 
+
+    I changed the background to have a hex value of #12D9CC, and now the contrast ratio is 11.82:1.
+
+Semantic HTML:
+    1. I'm able to traverse the navigation bar and search bar, but after that I'm immediately taken to the audio file, which skips a lot of the content present on the web page. Also, the navigation bar links and links on the side of the page don't appear to actually link to anything.
+
+    2. Yes, I can make this easier to navigate. I modifications I made were:
+        - Changed all <font> elements to <h1>, <h2>, or <h3> elements depending on the desired font size. Changed the corresponding <font> CSS properties to the corresponding header property.
+        - Modified text that was separated by <br> elements so they were contained by <p> elements instead. 
+        
+
+    3. This should be updated to a <nav> element. I have updated this in the HTML file, as well as the CSS properties that used to target the div element that was there to the new <nav> element
+
+The Images:
+    Yes, I have added 'alt' and 'title' attributes to the <img> elements.
+
+The Audio Player:
+    1. Yes, I've added a 'Show Transcript' button that allows the user to read the transcript of the audio file when pressed.
+
+    2. I've added a link to the text that shows up when someone is using a browser that doesn't support HTML5 audio players. This link gives them the audio file so they can download it and listen to it that way.
+
+The Forms:
+    1. I've added an 'aria-label' attribute to the search bar element that will say "Search through site content" when highlighted by a screen reader.
+
+    2. I've changed the elements surrounding the label text to <label> elements and given them the 'for' attributes so they're associated with their corresponding input elements. 
+
+The Show/Hide Comment Control:
+    Yes and yes. I gave the <div> element used as the Show/Hide Comment button the attribute 'tabindex' with a value of '0' so the keyboard is allowed to tab to it. I then added some javascript code that clicks the button whenever it's selected and the 'Enter' key is pressed.
+
+The Table:
+    Yes I can. I added the following to the table:
+    - Added a caption to the table
+    - Gave the titles of each column or row of the table the 'scope="col"' or 'scope="row"' attribute as necessary
+
+Other Considerations:
+    1. I changed the very first <div> element to a <header> element
+    2. I changed the buttons that activated the search bar and submitted a comment to actual <button> elements instead of <input> elements. 
--- js/main.js
+++ js/main.js
@@ -1,49 +1,70 @@
-/**
- * This file should only contain functions that don't interact with the DOM.
- * That means no document.querySelector, no document.getElementById, etc.
- * This file should only contain functions that do things like calculations,
- * data manipulation, etc. This is so that we can test these functions
- * without having to worry about the DOM or the browser environment.
- */
+// functionality for showing/hiding the comments section
 
-/**
- * This function prints the string 'Hello World' to the console
- */
-export function helloWorld() {
-    console.log('Hello World');
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
 }
 
-/**
- * This function adds two numbers together and returns the sum. This demonstrates
- * how to pass parameters to a function and return a value from a function
- * from HTML.
- * @param {number} a The first number to add
- * @param {number} b The second number to add
- * @returns {number} The sum of the two numbers
- */
-export function add(num1, num2) {
-    if (num1 === undefined || num2 === undefined) {
-        throw new Error('You must provide two numbers to add');
-    }
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        throw new Error('You must provide two numbers to add');
-    }
-    return num1 + num2;
-}
+// Functionality for show transcript button
 
-/**
- * This function fetches a random joke from the "Official Joke API" and returns it.
- * @returns {string} A joke in the format "setup - punchline"
- */
-export async function fetchRandomJoke() {
-    try {
-        const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-        if (!response.ok) {
-            throw new Error('Failed to fetch a joke');
-        }
-        const joke = await response.json();
-        return `${joke.setup} - ${joke.punchline}`;
-    } catch (error) {
-        throw new Error(error.message);
-    }
+const transcript = document.querySelector('.transcript');
+const transcriptBtn = document.querySelector('.transcript-container button');
+
+transcriptBtn.onclick = function() {
+  if(transcriptBtn.textContent === 'Show Transcript') {
+    transcript.style.height = '75px';
+    transcriptBtn.textContent = 'Hide Transcript';
+  } else {
+    transcript.style.height = '0';
+    transcriptBtn.textContent = 'Show Transcript';
+  }
+};
+
+// Enter/Return key functionality for the Show Comments button
+document.onkeydown = (e) => {
+  // The Enter/Return key
+  if(e.key === "Enter") {
+    document.activeElement.click();
+  }
 }
